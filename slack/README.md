3b1c8bf3822d41ad4dcaf60349796a1f62357b80
esdras <esdrasntuyenabo40@gmail.com>
2024-06-24 13:17:18 +0200

    authenticated and retrieve channel history

--- slack/README.md
+++ slack/README.md
@@ -7,13 +7,13 @@ Progress checklist
 working on
 -----------------------
 1. **Slack Account and API Key:**
-    - []  Create a slack account 
-    - []  Generate API Key
+    - [x]  Create a slack account 
+    - [x]  Generate Bot Token
     - []  Generate an OAuth token
 2. **Environment Setup:**
-    - []  Create a `.env` file in project directory.
-    - []  Add Trello API key and OAuth token to the `.env` file.
-    - []  Install the `godotenv` package for loading environment variables in Go.
+    - [x]  Create a `.env` file in project directory.
+    - []  Add bot token to the `.env` file.
+    - [x]  Install the `godotenv` package for loading environment variables in Go.
 3. **Testing Authentication:**
-    - []  Write a simple Go script to load the API key and token from the `.env` file.
-    - []  Make a test API call to Trello (e.g., get a list of boards) to ensure authentication is working
+    - [x]  Make a test API call to slack api (joining a conversation channel in this case) to ensure authentication is working
+    - [] retrieve messages from a target channel 
--- slack/main.go
+++ slack/main.go
@@ -1,8 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Calling Slack API")
+	//load .env
+	err := godotenv.Load()
 
+	if err != nil {
+		return
+	}
+	bot_token := os.Getenv("BOT_TOKEN")
+	main_end_point := os.Getenv("MAIN_END_POINT")
+	fmt.Println(bot_token)
+	fmt.Println(main_end_point)
+	channelID := "C06KPMXQS4U"
+	conversationHistory(bot_token, main_end_point, channelID)
+}
+
+func conversationHistory(bot_token, main_end_point, channelID string) {
+	//create url
+
+	url := fmt.Sprintf("%sconversations.history", main_end_point)
+	fmt.Println(url)
+	//create json payload for channelid
+	payload := map[string]string{"channel": channelID}
+	//marshal json
+	payloadByte, err := json.Marshal(payload)
+
+	if err != nil {
+		return
+	}
+	//new request
+	req, err := http.NewRequest("POST", url, bytes.NewReader(payloadByte))
+	if err != nil {
+		return
+	}
+	//set authorization header
+	req.Header.Set("Authorization", "Bearer "+bot_token)
+	req.Header.Set("Content-Type", "application/json")
+
+	//hangle client response
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal("error receiving response", err)
+	}
+
+	databyte, err := io.ReadAll(res.Body)
+	if err != nil {
+		return
+	}
+	//format json body data
+	var prettyJSON bytes.Buffer
+	err = json.Indent(&prettyJSON, databyte, "", " ")
+	if err != nil {
+		log.Fatal("Error formatting JSON: ", err)
+	}
+	fmt.Println(prettyJSON.String())
 }
