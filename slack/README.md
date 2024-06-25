245ec8c2459ac889432e1873f0c4d36b11282629
esdras <esdrasntuyenabo40@gmail.com>
2024-06-25 11:30:55 +0200

    wrote sendMessage function

--- slack/README.md
+++ slack/README.md
@@ -12,8 +12,9 @@ working on
     - []  Generate an OAuth token
 2. **Environment Setup:**
     - [x]  Create a `.env` file in project directory.
-    - []  Add bot token to the `.env` file.
+    - [x]  Add bot token to the `.env` file.
     - [x]  Install the `godotenv` package for loading environment variables in Go.
 3. **Testing Authentication:**
     - [x]  Make a test API call to slack api (joining a conversation channel in this case) to ensure authentication is working
-    - [] retrieve messages from a target channel 
+    - [x] retrieve messages from a target channel 
+    - [x] send messages to a target channel 
--- slack/main.go
+++ slack/main.go
@@ -25,7 +25,8 @@ func main() {
 	fmt.Println(bot_token)
 	fmt.Println(main_end_point)
 	channelID := "C06KPMXQS4U"
-	conversationHistory(bot_token, main_end_point, channelID)
+	newMEssage := "hello world from iTerm"
+	sendMessage(bot_token, main_end_point, channelID, newMEssage)
 }
 
 func conversationHistory(bot_token, main_end_point, channelID string) {
@@ -37,7 +38,6 @@ func conversationHistory(bot_token, main_end_point, channelID string) {
 	payload := map[string]string{"channel": channelID}
 	//marshal json
 	payloadByte, err := json.Marshal(payload)
-
 	if err != nil {
 		return
 	}
@@ -67,4 +67,42 @@ func conversationHistory(bot_token, main_end_point, channelID string) {
 		log.Fatal("Error formatting JSON: ", err)
 	}
 	fmt.Println(prettyJSON.String())
+}
+
+func sendMessage(bot_token, main_end_point, channelID, message string) {
+	//create url
+	url := fmt.Sprintf("%schat.postMessage", main_end_point)
+
+	//create json payload
+	payload := map[string]string{
+		"channel": channelID,
+		"text":    message,
+	}
+	///marshal json
+	payloadbyte, err := json.Marshal(payload)
+	if err != nil {
+		return
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewReader(payloadbyte))
+	if err != nil {
+		log.Fatal("Error sending request", err)
+	}
+	//set headers
+	req.Header.Set("Authorization", "Bearer "+bot_token)
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal("Error receiving respond", err)
+	}
+	//read respond
+	databyte, err := io.ReadAll(res.Body)
+	//format json body data
+	var prettyJSON bytes.Buffer
+	err = json.Indent(&prettyJSON, databyte, "", " ")
+	if err != nil {
+		log.Fatal("Error formatting JSON: ", err)
+	}
+	fmt.Println(prettyJSON.String())
+
 }
