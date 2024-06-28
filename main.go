0d7c73fe06bc6d3c6f3e10c76eee193bd3b713e9
esdras <esdrasntuyenabo40@gmail.com>
2024-06-28 21:16:49 +0200

    refactoring

--- main.go
+++ main.go
@@ -9,7 +9,14 @@ import (
 func main() {
 	fmt.Println("Testing from cmd")
 
-	//Testing slack api
-	config.Load_config()
+	//load .env
+	configs, err := config.Load_config()
+	if err != nil {
+		return
+	}
+	//loading environment variables for slack
+	channelID := "C06KPMXQS4U"
+	newMessage := "sending from branch v.1.0.1"
+	slack.SendMessage(configs.BOT_TOKEN, configs.SLACK_MAIN_END_POINT, channelID, newMessage)
 
 }
