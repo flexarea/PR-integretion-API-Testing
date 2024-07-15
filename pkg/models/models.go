29d1274969aa4c28e0ec4905fc15d96fac4fdd84
esdras <esdrasntuyenabo40@gmail.com>
2024-07-15 11:21:56 +0200

    created model

---
+++ pkg/models/models.go
@@ -0,0 +1,18 @@
+package pkg
+
+import (
+	"errors"
+	"time"
+)
+
+var err = errors.New("error")
+
+type PR_INFO struct {
+	ID                int
+	Title             string
+	Branch            string
+	DestinationBranch string
+	PR_Comment        string
+	SlackChannel      string
+	Created           time.Time
+}
