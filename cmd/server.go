53fa908361ddb607e2d0b4aba37bffb214370900
esdras <esdrasntuyenabo40@gmail.com>
2024-07-15 11:32:38 +0200

    added neon config template

--- cmd/server.go
+++ cmd/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/lib/pq"
 	"log"
 	"net/http"
@@ -32,4 +33,17 @@ func Server() {
 	infoLog.Printf("starting server on port %s", port)
 	err := server.ListenAndServe()
 	errorLog.Fatal(err)
+
+	connStr := "postgres://[user]:[password]@[neon_hostname]/[dbname]?sslmode=require"
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	var version string
+	if err := db.QueryRow("select version()").Scan(&version); err != nil {
+		panic(err)
+	}
+	fmt.Printf("version=%s\n", version)
 }
