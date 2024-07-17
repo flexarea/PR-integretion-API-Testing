83ab05509401c1283d41f1dcc331dbb0c5f1a609
esdras <esdrasntuyenabo40@gmail.com>
2024-07-17 12:41:48 +0200

    configuring db operations

--- pkg/models/logs.go
+++ pkg/models/logs.go
@@ -3,3 +3,17 @@ package models
 import (
 	"database/sql"
 )
+
+type LogsModel struct {
+	DB *sql.DB
+}
+
+func (m *LogsModel) Insert(title, branch, destinationBranch, Pr_comment, slackchannel string) (int, error) {
+	stmt := `INSERT INTO prinfo (title, branch, destinationBranch, pr_comment, slackchannel, created)
+	VALUES ($1,$2,$3,$4,$5,NOW() AT TIME ZONE 'Africa/Kigali')
+	`
+
+	//executing query
+	result, err := m.DB.Exec(stmt, title, branch, destinationBranch, Pr_comment, slackchannel)
+
+}
