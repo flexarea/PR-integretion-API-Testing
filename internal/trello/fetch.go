7388614d5467c0e9d66cb639d254ef9313993f60
esdras <esdrasntuyenabo40@gmail.com>
2024-07-18 16:04:00 +0200

    writing fetch

--- internal/trello/fetch.go
+++ internal/trello/fetch.go
@@ -2,22 +2,19 @@ package trello
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 )
 
-func Retrieve(data string) {
+func Retrieve(cardShortID interface{}, data string) {
 
 	var jsonData = []byte(data)
 
 	var result []map[string]interface{}
 
-	err := json.Unmarshal(jsonData, result)
+	err := json.Unmarshal(jsonData, &result)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Printf("%T", result)
 
 }
